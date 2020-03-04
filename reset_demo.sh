3042d41926439f04872665a5fa7f6f2307f4c0ea
Heiko Zuerker <heiko@zuerker.org>
2020-03-04 09:44:16 -0600

    variable corrections and script cleanup

--- reset_demo.sh
+++ reset_demo.sh
@@ -13,8 +13,16 @@ gcloud container clusters get-credentials next-gitops-demo --zone us-central1-a 
 
 mkdir -p tmp
 
+# Nuke the config connector CRs
+helm template templates/google-config-connector/ -f configs/demo/networks.yaml \
+  | kubectl delete -f -
+
 # Remove the namespace and everything in it
-helm template templates/eunomia-cr/ --set projectName="${PROJECT_ID}" --set git.uri="${URI}" --set git.ref="${REF}" | kubectl delete -f -
+helm template templates/eunomia-cr/ -f configs/demo/networks.yaml \
+  --set projectName="${PROJECT_ID}" \
+  --set git.uri="${URI}" \
+  --set git.ref="${REF}" \
+  | kubectl delete -f -
 
 # Restart Eunomia
 kubectl delete pods --all -n eunomia-operator
--- run_demo.sh
+++ run_demo.sh
@@ -11,8 +11,16 @@ gcloud config set project ${PROJECT_ID}
 
 gcloud container clusters get-credentials next-gitops-demo --zone us-central1-a --project ${PROJECT_ID}
 
-# create the GitOpsConfig CR
-helm template templates/eunomia-cr/ --set projectName="${PROJECT_ID}" --set git.uri="${URI}" --set git.ref="${REF}" | kubectl apply -f -
+# create the namespace and GitOpsConfig CR
+helm template templates/eunomia-cr/ -f configs/demo/networks.yaml \
+  --set projectName="${PROJECT_ID}" \
+  --set git.uri="${URI}" \
+  --set git.ref="${REF}" \
+  | kubectl apply -f -
+
+# create the google config connector CRs
+#helm template templates/google-config-connector/ -f configs/demo/networks.yaml \
+#  | kubectl apply -f -
 
 kubectl get pods --all-namespaces
 
