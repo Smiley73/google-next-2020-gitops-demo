96421f6cc710f9b4406defc9e2376e7300940ae8
Heiko Zuerker <heiko@zuerker.org>
2020-03-04 09:52:32 -0600

    simplify resetting

--- reset_demo.sh
+++ reset_demo.sh
@@ -1,28 +1,18 @@
 #!/bin/bash
 
-set -euo pipefail
-
 # Nothing special here. We're just automatically setting up the requirements as documented by Eunomia:
 # https://github.com/KohlsTechnology
 
 echo "Using GCP project '${PROJECT_ID}'"
 
-gcloud config set project ${PROJECT_ID}
+gcloud config set project ${PROJECT_ID} || exit 1
 
-gcloud container clusters get-credentials next-gitops-demo --zone us-central1-a --project ${PROJECT_ID}
+gcloud container clusters get-credentials next-gitops-demo --zone us-central1-a --project ${PROJECT_ID}  || exit 1
 
 mkdir -p tmp
 
-# Nuke the config connector CRs
-helm template templates/google-config-connector/ -f configs/demo/networks.yaml \
-  | kubectl delete -f -
-
-# Remove the namespace and everything in it
-helm template templates/eunomia-cr/ -f configs/demo/networks.yaml \
-  --set projectName="${PROJECT_ID}" \
-  --set git.uri="${URI}" \
-  --set git.ref="${REF}" \
-  | kubectl delete -f -
+# Nuke the namespace
+kubectl delete namespace next-gitops-demo
 
 # Restart Eunomia
 kubectl delete pods --all -n eunomia-operator
