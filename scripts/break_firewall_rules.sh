32d478ea2c550b8f7d8f5cb90a6536be25f54b93
Heiko Zuerker <heiko.zuerker@kohls.com>
2020-03-05 16:04:55 -0600

    minor cleanup

--- scripts/break_firewall_rules.sh
+++ scripts/break_firewall_rules.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 
-# Nothing special here. We're just automatically setting up the requirements as documented by Eunomia:
-# https://github.com/KohlsTechnology
-
-echo "Using GCP project '${PROJECT_ID}'"
-
-gcloud config set project ${PROJECT_ID} || exit 1
-
 gcloud compute firewall-rules delete next-gitops-demo-1-allow-icmp -q
