6af597efd4abbfecf6de8e7a883674f34154220f
Heiko Zuerker <heiko.zuerker@kohls.com>
2020-03-05 15:53:16 -0600

    fix k8s login timing

--- scripts/configure_env.sh
+++ scripts/configure_env.sh
@@ -4,7 +4,5 @@ echo "Using GCP project '${PROJECT_ID}'"
 
 gcloud config set project ${PROJECT_ID} || exit 1
 
-gcloud container clusters get-credentials next-gitops-demo --zone us-central1-a --project ${PROJECT_ID}  || exit 1
-
 echo "Using Git Repo: ${URI}"
 echo "Using Git Branch: ${REF}"
--- scripts/setup_gke.sh
+++ scripts/setup_gke.sh
@@ -13,13 +13,14 @@ gcloud beta container clusters create next-gitops-demo \
   --release-channel=stable
 #  --preemptible \
 
+
 # Access for workload identity
 gcloud iam service-accounts add-iam-policy-binding \
 cnrm-system@${PROJECT_ID}.iam.gserviceaccount.com \
 --member="serviceAccount:${PROJECT_ID}.svc.id.goog[cnrm-system/cnrm-controller-manager]" \
 --role="roles/iam.workloadIdentityUser"
 
-# Make sure we can log in
+# Make sure kubectl is configured for us to use the new cluster
 gcloud container clusters get-credentials next-gitops-demo \
   --zone us-central1-a --project ${PROJECT_ID}
 
